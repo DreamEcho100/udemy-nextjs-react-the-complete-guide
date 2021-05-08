c2dc65028c3a12286b379318114f4567beece499
DreamEcho100 <maze6572198@gmail.com>
2021-05-08 20:31:01 +0200

    programmatic navigation after search

--- components/events/EventsSearch/index.jsx
+++ components/events/EventsSearch/index.jsx
@@ -8,21 +8,34 @@ const classes = {
 import './EventsSearch.scss';
 
 import Button from '@/components/ui/Button/index';
+import { useRef } from 'react';
 
-const EventsSearch = () => {
+const EventsSearch = ({ onSearch }) => {
+	const yearInputRef = useRef();
+	const monthInputRef = useRef();
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+
+		const selectedYear = yearInputRef.current.value;
+		const selectedMonth = monthInputRef.current.value;
+
+		onSearch(selectedYear, selectedMonth);
+	};
+
 	return (
-		<form className={classes.form}>
+		<form className={classes.form} onSubmit={submitHandler}>
 			<div className={classes.controls}>
 				<div className={classes.control}>
 					<label htmlFor='year'>Year</label>
-					<select id='year'>
+					<select id='year' ref={yearInputRef}>
 						<option value='2021'>2021</option>
 						<option value='2022'>2022</option>
 					</select>
 				</div>
 				<div className={classes.control}>
 					<label htmlFor='month'>Month</label>
-					<select id='month'>
+					<select id='month' ref={monthInputRef}>
 						<option value='1'>January</option>
 						<option value='2'>February</option>
 						<option value='3'>March</option>
--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,14 +1,23 @@
+import { useRouter } from 'next/router';
+
 import { getAllEvents } from '../../dummy-data';
 
 import EventList from '@/components/events/EventList/index';
 import EventsSearch from '@/components/events/EventsSearch/index';
 
 const AllEventsPage = () => {
+	const router = useRouter();
 	const events = getAllEvents();
+
+	const findEventsHandler = (year, month) => {
+		const fullPath = `/events/${year}/${month}`;
+
+		router.push(fullPath);
+	};
 
 	return (
 		<div>
-			<EventsSearch />
+			<EventsSearch onSearch={findEventsHandler} />
 			<EventList items={events} />
 		</div>
 	);
