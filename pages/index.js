2981ac48fc0e150c6d5b2fa116c15c5a789f147f
DreamEcho100 <maze6572198@gmail.com>
2021-05-12 20:00:42 +0200

    fixing bugs

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+const Home = () => {
+	return (
+		<section>
+			<h1>Home Page</h1>
+		</section>
+	);
+};
 
-export default function Home() {
-  return (
-    <section>
-      <h></h>
-    </section>
-  );
-}
+export default Home;
