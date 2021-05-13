3bec9e811d176dd475fac45e9cedd408b41d719e
DreamEcho100 <maze6572198@gmail.com>
2021-05-13 06:07:50 +0200

    fetching data for the homepage

---
+++ helpers/api-util.js
@@ -0,0 +1,29 @@
+export const getAllEvents = async () => {
+	const events = [];
+
+	await fetch(
+		'https://udemy-nextjs-course-full-guide-default-rtdb.firebaseio.com/events.json'
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			let key;
+			for (key in data) {
+				events.push({
+					id: key,
+					...data[key],
+				});
+			}
+		})
+		.catch((error) => {
+			console.error(error);
+			return;
+		});
+
+	return events;
+};
+
+export const getFeaturedEvents = async () => {
+	return await getAllEvents().then((events) =>
+		events.filter((event) => event.isFeatured)
+	);
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,24 @@
 import { getFeaturedEvents } from '@/dummy-data';
+// import { getAllEvents } from '../helpers/api-util';
+
 import EventList from '@/components/events/EventList/index';
 
-const index = () => {
-	const featuredEvents = getFeaturedEvents();
-
+const index = ({ events }) => {
 	return (
 		<div>
-			<EventList items={featuredEvents} />
+			<EventList items={events} />
 		</div>
 	);
 };
 
 export default index;
+
+export const getStaticProps = async () => {
+	const featuredEvents = getFeaturedEvents();
+
+	return {
+		props: {
+			events: featuredEvents,
+		},
+	};
+};
