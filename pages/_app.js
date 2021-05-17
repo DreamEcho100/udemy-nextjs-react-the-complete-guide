e0caa6dd43a990cdff2e845024bcc3132d7249ee
DreamEcho100 <maze6572198@gmail.com>
2021-05-18 00:08:36 +0200

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import { Fragment } from 'react';
+import Head from 'next/head';
+
+import '@/styles/_globals.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Fragment>
+    <Head>
+      <meta charSet='UTF-8' />
+      <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+      <meta name='robots' content='index,follow' />
+      <meta
+        name='viewport'
+        content='width=device-width, initial-scale=1.0'
+      />
+      <title>Next Events</title>
+    </Head>
+    <Component {...pageProps} />
+  </Fragment>)
 }
 
 export default MyApp
